2b0616ab42acb1d8fcde20afc7da5856b516322c
FredHSQ <frederico.shere@gmail.com>
2022-06-22 16:02:35 -0300

    feat: adicionando async storage

--- my-skills/src/contexts/CartContext/index.tsx
+++ my-skills/src/contexts/CartContext/index.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, useState, FC, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { magicItemList } from '../../pages/Shop';
 
@@ -22,24 +23,51 @@ export const CartContext = createContext<ICardContext>({
 
 export const CartProvider: FC<ICardContext> = ({ children }) => {
     const [magicItemList, setMagicItemList] = useState<magicItemList[]>([]);
-    const [precoTotal, setPrecoTotal] =useState<number>(0);
+    const [precoTotal, setPrecoTotal] = useState<number>(0);
 
-    useEffect(()=>{
+    const storeData = async (value: magicItemList[]) => {
+        try {
+            const jsonValue = JSON.stringify(value)
+            await AsyncStorage.setItem('@storage_Key', jsonValue)
+        } catch (e) {
+            // saving error
+        }
+    }
+    const getData = async () => {
+        try {
+            const jsonValue = await AsyncStorage.getItem('@storage_Key')
+            return jsonValue != null ? JSON.parse(jsonValue) : null;
+        } catch (e) {
+            // error reading value
+        }
+    }
+
+    useEffect(() => {
+        getData().then((res)=>{
+            setMagicItemList(res);
+        })
+    },[])
+
+    useEffect(() => {
         let soma = 0;
-        magicItemList.map((magicItem: magicItemList)=>{
+        magicItemList.map((magicItem: magicItemList) => {
             soma = soma + Number(magicItem.preco)
         });
         setPrecoTotal(soma);
-    },[magicItemList]);
+    }, [magicItemList]);
 
-    const addMagicItem = (magicItem:magicItemList) => {
+    const addMagicItem = (magicItem: magicItemList) => {
         setMagicItemList([...magicItemList, magicItem]);
+        storeData([...magicItemList, magicItem])
     };
-    
+
     const removeMagicItem = (index: string) => {
-        setMagicItemList(magicItemList.filter((magicItemList: magicItemList)=> {
+        let newMagicItemList = magicItemList.filter((magicItemList: magicItemList) => {
             return magicItemList.index !== index
-        }));
+        })
+
+        setMagicItemList(newMagicItemList);
+        storeData(newMagicItemList);
     };
 
     return (
